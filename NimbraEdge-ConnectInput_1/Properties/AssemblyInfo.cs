b23bb9677beffbe7cea55e26c00bdaa01c7c9210
Flavio Meneses <121137893+fsmeneses@users.noreply.github.com>
2023-02-27 12:07:10 +0000

    init script project

---
+++ NimbraEdge-ConnectInput_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NimbraEdge-ConnectInput_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("NimbraEdge-ConnectInput_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("970935FF-B456-4225-93FB-415171DCB59C")]
+[assembly: AssemblyVersion("1.0.0.0")]
